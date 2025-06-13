2528e11dd7b0a83347c83f5e9832895b3b090c50
compudefierro <jorgeeldri@gmail.com>
2025-06-13 19:11:19 -0300

    added while loop and if ternary True False

--- ch03/tip_calculator.py
+++ ch03/tip_calculator.py
@@ -1,10 +1,14 @@
-print("Welcome to the tip calculator.")
-total_bill = float(input("What was the total bill?>"))
-ppl = int(input("How many people to split the bill?>"))
-tip = int(input("What percentage tip would you like to give?>"))
-percentage_tip = (tip / 100) + 1
-total_tip = total_bill * (tip / 100)
-total_final = total_bill * percentage_tip
-total_each_ppl = total_final / ppl
-print(f"Total bill: {total_bill:.2f}\nTotal ppl: {ppl}\nPercentage tip: %{tip}\nTotal Tip: {total_tip:.2f}\nTotal with bill: {total_final:.2f}\nTotal each ppl: {total_each_ppl:.2f}")
-
+while True:
+    print("Welcome to the tip calculator.")
+    total_bill = float(input("What was the total bill?>"))
+    ppl = int(input("How many people to split the bill?>"))
+    tip = int(input("What percentage tip would you like to give?>"))
+    percentage_tip = (tip / 100) + 1
+    total_tip = total_bill * (tip / 100)
+    total_final = total_bill * percentage_tip
+    total_each_ppl = total_final / ppl
+    print(f"Total bill: {total_bill:.2f}\nTotal ppl: {ppl}\nPercentage tip: %{tip}\nTotal Tip: {total_tip:.2f}\nTotal with bill: {total_final:.2f}\nTotal each ppl: {total_each_ppl:.2f}")
+    continuar = True if input("Continuar? (N para salir)>") == "N" else False
+    if continuar:
+        break
+    
