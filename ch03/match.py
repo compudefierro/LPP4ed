e9929107626d53f2fbf1851958a9052ef3ab767c
compudefierro <jorgeeldri@gmail.com>
2025-06-13 18:58:41 -0300

    match.py added loop while and if break

--- ch03/match.py
+++ ch03/match.py
@@ -1,11 +1,17 @@
 # match.py
-day_number = 1
-match day_number:
-    case 1 | 2 | 3 | 4 | 5:
-        print("Weekday")
-    case 6:
-        print("Saturday")
-    case 7:
-        print("Sunday")
-    case _:
-        print(f"{day_number} is not a valid day number")
+import random
+
+while True:
+    day_number = random.randint(0,8)
+    match day_number:
+        case 1 | 2 | 3 | 4 | 5:
+            print("Weekday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _:
+            print(f"{day_number} is not a valid day number")
+    continuar = True if input("continuar?") == "1" else False
+    if continuar:
+        break
