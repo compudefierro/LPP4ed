424da72ca86dd8839d28bedc01ca95e1a6efe02e
compudefierro <jorgeeldri@gmail.com>
2025-08-09 09:11:28 -0300

    feet to meters

--- ch03/practica_tk/firstExample_feet_to_meters.py
+++ ch03/practica_tk/firstExample_feet_to_meters.py
@@ -10,6 +10,12 @@ def calculate(*args):
     except ValueError:
         pass
     
+def clearInput(*args):
+    feet.set("")
+    
+def closeWindows(*args):
+    root.destroy()
+    
 root = Tk()
 root.title("Feet to Meters")
 
@@ -26,16 +32,18 @@ feet_entry.grid(column = 2, row = 1, sticky=(W, E))
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column = 2, row = 2, sticky=(W, E))
 
-ttk.Button(mainframe, text="Calculate", command = calculate).grid(column = 3, row = 3, sticky = W)
-
 ttk.Label(mainframe, text = "feet").grid(column = 3, row = 1, sticky = W)
 ttk.Label(mainframe, text = "is equivalent to").grid(column  = 1, row = 2, sticky = E)
 ttk.Label(mainframe, text = "meters").grid(column = 3, row = 2, sticky = W)
+
+ttk.Button(mainframe, text="Calculate", command = calculate).grid(column = 4, row = 4, sticky = W)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx = 5, pady = 5)
     
 feet_entry.focus()
 root.bind("<Return>", calculate)
+root.bind("<F1>", clearInput)
+root.bind("<q>", closeWindows)
 
 root.mainloop()
